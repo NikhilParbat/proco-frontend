12339e201e5e460517de0ce146e2b51add66f2d7
Hemil Kothari <hemilkothari35@gmail.com>
2026-05-17 00:04:43 +0530

    chore: restricted roation of app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:proco/app_initializer.dart';
 import 'package:proco/firebase_options.dart';
+import 'package:flutter/services.dart';
 
 @pragma('vm:entry-point')
 Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
@@ -12,6 +13,11 @@ Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown, // Optional: allows upside-down portrait
+  ]);
 
   await dotenv.load(fileName: '.env');
 
